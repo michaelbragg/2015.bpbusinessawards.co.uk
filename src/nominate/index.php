b8eb9007f1d687d7edd69571fc2c48dda1bc0aa1
Michael Bragg <michael@michaelbragg.net>
2015-07-09 10:26:52 +0100

    Add base nominations page

---
+++ src/nominate/index.php
@@ -0,0 +1,27 @@
+---
+layout: php
+stylesheet:
+javascript:
+class: page__nominate not-hero
+
+title: Nominations
+permalink: nominate/index.php
+meta:
+  keywords:
+  description:
+  author:
+
+hero: false
+partners: true
+
+sitemap:
+  lastmod: 2015-05-06
+  priority: 0.7
+  changefreq: weekly
+  exclude: false
+---
+{% if site.nominate %}
+{% raw  %}<?php include HOME . '/' . $page; ?>{% endraw %}
+{% else %}
+<p>Nominations have now closed. Stay tuned for the shortlist to be announced.</p>
+{% endif %}
